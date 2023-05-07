27fdb1e8f926332cffaaac9010f481562b964d70
David Achoy <david@achoy.net>
2023-05-07 13:10:10 -0600

    cambios

--- src/app/game-info/game-info.component.ts
+++ src/app/game-info/game-info.component.ts
@@ -1,11 +1,81 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { environment } from 'src/environments/environment';
+import { ConnectionService } from '../connection/connection';
+import { Response } from '../connection/response';
+import { CookieService } from 'ngx-cookie-service';
 
 @Component({
   selector: 'app-game-info',
   templateUrl: './game-info.component.html',
-  styleUrls: ['./game-info.component.css'],
+  styleUrls: ['../../styles.css'],
 })
 export class GameInfoComponent {
+  constructor(
+    private cs: ConnectionService,
+    private cookies: CookieService,
+    private router: Router
+  ) {}
+  genreList = [
+    'Action',
+    'Platform',
+    'Shooter',
+    'Fighting',
+    "Beat'em'up",
+    'Stealth',
+    'Survival',
+    'Rhythm',
+    'Action-adventure',
+    'Survival horror',
+    'Metroidvania',
+    'Adventure',
+    'Text adventure',
+    'Graphic adventure',
+    'Visual Novel',
+    'Interactive Movie',
+    'RPG',
+    'Action-RPG',
+    'Roguelike',
+    'Tactical RPG',
+    'Sandbox',
+    'JRPG',
+    'Simulation',
+    'Strategy',
+    'Sports',
+    'Puzzle',
+    'Idle',
+    'Party',
+    'Digital Card game',
+    'Logic',
+    'Horror',
+    'Trivia',
+    'Typing',
+    'Single player',
+    'Multiplayer',
+    'Competitive',
+    'Co-op',
+  ];
+
+  engineList = [
+    'Unity',
+    'Unreal',
+    'Game Maker',
+    'Construct',
+    'Amazon Lumberyard',
+    'CryEngine',
+    'Godot',
+    'Cocos2d',
+    'RPG Maker',
+  ];
+
+  platformList = [
+    'Mobile platforms (like iOS and Android)',
+    'Desktop platforms (Windows, Mac and Linux)',
+    'Web platform (WebGL)',
+    'Console platforms (like PS4, PS5 and Xbox)',
+    'Virtual/Extended reality platforms (like Oculus and PlayStation VR)',
+  ];
+
   selectedThemes: string[] = [];
   addSelectedThemes(): void {
     const selectElement = document.getElementById(
@@ -19,7 +89,6 @@ export class GameInfoComponent {
         this.selectedThemes.push(theme);
       }
     });
-
   }
   removeTheme(theme: string): void {
     const themeIndex = this.selectedThemes.indexOf(theme);
--- src/app/team-info/team-info.component.ts
+++ src/app/team-info/team-info.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-team-info',
   templateUrl: './team-info.component.html',
-  styleUrls: ['./team-info.component.css']
+  styleUrls: ['../../styles.css']
+
 })
 export class TeamInfoComponent {
   fileName: string = 'Choose file';
