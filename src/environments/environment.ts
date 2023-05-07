a0830253f7a4740db213f5f86f57c4e970abca6a
Jose Sequeira <jsc190@gmail.com>
2023-05-07 02:57:42 -0600

    Added env variables

---
+++ src/environments/environment.ts
@@ -0,0 +1,8 @@
+export const environment = {
+    BACKEND: "http://localhost:4000",
+    BIRTH_ERROR: "You must be 18 or older to participate!",
+    EMAIL_ERROR: "You must type a valid email!\tExpected format: user@example.com",
+    EMAIL_REGEX: /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})$/,
+    PHONE_ERROR: "You must provide a valid phone number!\tExpected format: +1 234567890",
+    PHONE_REGEX: /^\+\d{1,3}\s\d{1,14}$/,
+};
