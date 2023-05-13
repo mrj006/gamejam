776862d1c7ed24a70e7fe3a213ecbdf6dcad410e
Jose Sequeira <jsc190@gmail.com>
2023-05-13 13:56:04 -0600

    Updated controller to handle composed models

--- backend/controllers/gamejam.js
+++ backend/controllers/gamejam.js
@@ -1,13 +1,90 @@
 const GameJam = require("../models/gamejam");
 
 module.exports = class Controller{
-    static getCurrentGameJam() {
-        GameJam.findOne({}).sort('-date')
-            .then(result => {
-                return result._id;
-            }).catch(e => {
-                console.error(e);
-                return false;
+    static async getCurrentGameJam() {
+        try {
+            let gamejam = await GameJam.findOne({}).sort('-_id');
+
+            if (!gamejam) {
+                return null;
+            }
+
+            return gamejam;
+        } catch(e) {
+            console.error(e);
+            return null;
+        }
+    };
+
+    static getCurrentGameJamRoute = async (req, res) => {
+        try {
+            let gamejam = await GameJam.findOne({}).sort('-_id');
+
+            if (!gamejam) {
+                return res.status(404).send({
+                    message: "We couldn't find the information you are looking for.",
+                    code: 404,
+                });
+            }
+
+            return res.send({
+                code: 200,
+                data: [gamejam],
             });
+        } catch(e) {
+            console.error(e);
+            return res.status(500).send({
+                message: "An error occured while fetching information! Try again later.",
+                code: 500,
+            });
+        }
     }
+
+    static getCurrentCategories = async (req, res) => {
+        try {
+            let gamejam = await GameJam.findOne({}).sort('-_id');
+
+            if (!(gamejam && gamejam.categories)) {
+                return res.status(404).send({
+                    message: "We couldn't find the information you are looking for.",
+                    code: 404,
+                });
+            }
+
+            return res.send({
+                code: 200,
+                data: gamejam.categories,
+            });
+        } catch(e) {
+            console.error(e);
+            return res.status(500).send({
+                message: "An error occured while fetching information! Try again later.",
+                code: 500,
+            });
+        }
+    };
+
+    static getCurrentThemes = async (req, res) => {
+        try {
+            let gamejam = await GameJam.findOne({}).sort('-_id');
+
+            if (!(gamejam && gamejam.themes)) {
+                return res.status(404).send({
+                    message: "We couldn't find the information you are looking for.",
+                    code: 404,
+                });
+            }
+
+            return res.send({
+                code: 200,
+                data: gamejam.themes,
+            });
+        } catch(e) {
+            console.error(e);
+            return res.send({
+                message: "An error occured while fetching information! Try again later.",
+                code: 500,
+            });
+        }
+    };
 };
