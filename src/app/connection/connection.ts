9679ee6d71f100c7d0125ef5d6357cd6ecf7849f
Jose Sequeira <jsc190@gmail.com>
2023-05-13 13:52:52 -0600

    Added routes and token handling

--- src/app/connection/connection.ts
+++ src/app/connection/connection.ts
@@ -3,14 +3,18 @@ import { HttpClient } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 import { User } from '../models/user.model';
 import { Game } from '../models/game.model';
+import { Response } from './response';
+import { CookieService } from 'ngx-cookie-service';
+
 @Injectable({
     providedIn: 'root'
 })
 
 export class ConnectionService {
     private api = environment.BACKEND;
+    private token = this.cookies.get("token");
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient, private cookies: CookieService) {}
 
     /////////////////////////////// USER ///////////////////////////////
 
@@ -18,19 +22,49 @@ export class ConnectionService {
         return this.http.post(this.api + "/register", user);
     }
 
+    public findUser(param: string) {
+        return this.http.get<Response>(`${this.api}/findUsers?user=${param}`);
+    }
+
     /////////////////////////////// GAME ///////////////////////////////
 
-    public findUser(param: string) {
-        return this.http.get(`${this.api}/find-users?user=${param}`);
+    public getCurrentCategories() {
+        return this.http.get<Response>(this.api + "/currentCategories");
     }
-    public uploadFirstStage(game: Game) {
-        return this.http.post(this.api + "/first-stage", game);
+    
+    public getCurrentThemes() {
+        return this.http.get<Response>(this.api + "/currentThemes");
     }
+
+    public getCurrentVenues() {
+        return this.http.get<Response>(this.api + "/currentVenues");
+    }
+
+    public getEngines() {
+        return this.http.get<Response>(this.api + "/engines");
+    }
+
+    public getGenres() {
+        return this.http.get<Response>(this.api + "/genres");
+    }
+
+    public getPlatforms() {
+        return this.http.get<Response>(this.api + "/platforms");
+    }
+
     public getUserGames(param: string) {
-        return this.http.get(`${this.api}/getUserGames?user=${param}`);
+        return this.http.get<Response>(`${this.api}/getUserGames?user=${param}`);
     }
-    public uploadTeamInfo(game: Game, file :File) {
-        let body = {...game, file}
-        return this.http.post(this.api + "/teamInfo", body);
+
+    public uploadFirstStage(game: Game) {
+        return this.http.post<Response>(this.api + "/firstStage?token=" + this.token, game);
+    }
+
+    public uploadTeamInfo(form: FormData) {
+        return this.http.post<Response>(this.api + "/teamInfo?token=" + this.token, form);
+    }
+
+    public uploadGameInfo(form: FormData) {
+        return this.http.post<Response>(this.api + "/gameInfo?token=" + this.token, form);
     }
 }
