04b25deae0054b3f304a8caa6af8096342a848f7
Boddah98 <67991470+Boddah98@users.noreply.github.com>
2023-06-08 00:49:49 -0600

    Cleaning files

--- src/app/engine/engine.component.ts
+++ src/app/engine/engine.component.ts
@@ -15,23 +15,6 @@ export class EngineComponent implements OnInit {
     }
 
     init() {
-        document.getElementById("firstStage")?.addEventListener('click', evt => {
-            this.router.navigate(['/first-stage']);
-        });
-
-        document.getElementById("teamInfo")?.addEventListener('click', evt => {
-            this.router.navigate(['/team-info']);
-        });
-
-        document.getElementById("gameInfo")?.addEventListener('click', evt => {
-            this.router.navigate(['/game-info']);
-        });
-
-        document.getElementById("register")?.addEventListener('click', evt => {
-            this.router.navigate(['/register']);
-        });
-        document.getElementById("login")?.addEventListener('click', evt => {
-            this.router.navigate(['/login']);
-        });
+       
     }
 }
