7278db2ff7281ae08d4566d1b6502d279d6dfc7e
Jose Sequeira <jsc190@gmail.com>
2023-06-09 23:15:57 -0600

    Fixed bug

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -3,25 +3,25 @@ const jwt = require("jsonwebtoken");
 const { TOKEN_KEY } = process.env;
 
 const verifyToken = (req, res, next) => {
-  const token = req.body.token || req.query.token || req.cookies["token"];
+    const token = req.body.token || req.query.token || req.cookies["token"];
 
-  if (!token) {
-    return res.send({
-      message: "A token is required for authentication",
-      code: 403,
-    });
-  }
-  
-  try {
-    const decoded = jwt.verify(token, TOKEN_KEY);
-    req.user = decoded;
-  } catch (err) {
-    return res.send({
-      message: "Invalid Token",
-      code: 401
-    });
-  }
-  return next();
+    if (!token) {
+        return res.send({
+            message: "A token is required for authentication",
+            code: 403,
+        });
+    }
+
+    try {
+        const decoded = jwt.verify(token, TOKEN_KEY);
+        req.user = decoded.payload._id;
+    } catch (err) {
+        return res.send({
+            message: "Invalid Token",
+            code: 401
+        });
+    }
+    return next();
 };
 
 module.exports = verifyToken;
